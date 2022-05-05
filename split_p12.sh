97d7c43d98c9e3f476556a60322905a90ccf818c
jpdemory <demory@mailo.fr>
2022-05-05 13:37:05 +0200

    Ajout du mot de passe

--- split_p12.sh
+++ split_p12.sh
@@ -1,3 +1,5 @@
-openssl pkcs12 -in sio.manastria.ovh.pfx -out keyStore.pem -nodes
-openssl pkcs12 -in sio.manastria.ovh.pfx -out key.pem -nodes -nocerts
-openssl pkcs12 -in sio.manastria.ovh.pfx -out certificat.pem -nokeys
+# Étape 1
+
+openssl pkcs12 -in sio.manastria.ovh.pfx -out keyStore.pem -nodes -passin pass:netlab123
+openssl pkcs12 -in sio.manastria.ovh.pfx -out key.pem -nodes -nocerts -passin pass:netlab123
+openssl pkcs12 -in sio.manastria.ovh.pfx -out certificat.pem -nokeys -passin pass:netlab123
