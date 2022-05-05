8c3560cb20bd9bd624917e03b0c970d9de9b3ed6
jpdemory <demory@mailo.fr>
2022-05-05 14:17:25 +0200

    Remplacement des noms de fichier par des variables

--- split_p12.sh
+++ split_p12.sh
@@ -1,5 +1,16 @@
-# Étape 1
+# Étape 2
 
-openssl pkcs12 -in sio.manastria.ovh.pfx -out keyStore.pem -nodes -passin pass:netlab123
-openssl pkcs12 -in sio.manastria.ovh.pfx -out key.pem -nodes -nocerts -passin pass:netlab123
-openssl pkcs12 -in sio.manastria.ovh.pfx -out certificat.pem -nokeys -passin pass:netlab123
+filein=sio.manastria.ovh.pfx
+
+
+in_filename=$(basename -- "$filein")
+in_extension="${in_filename##*.}"
+in_filename="${in_filename%.*}"
+
+
+fileout=${in_filename}
+
+
+openssl pkcs12 -in "${filein}" -out ${fileout}_both.pem -nodes -passin pass:netlab123
+openssl pkcs12 -in "${filein}" -out ${fileout}_key.pem -nodes -nocerts -passin pass:netlab123
+openssl pkcs12 -in "${filein}" -out ${fileout}_cert.pem -nokeys -passin pass:netlab123
